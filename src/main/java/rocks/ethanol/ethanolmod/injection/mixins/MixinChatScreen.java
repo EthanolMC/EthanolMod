b3bdf4b21a83c53277e03dd0ce2371d0f2f8d5c7
NekosAreKawaii <225373546+NekosAreKawaii@users.noreply.github.com>
2024-07-06 20:10:17 +0200

    Added setting to increase the max chat field input length Preventing users from using '/' as command prefix

--- src/main/java/rocks/ethanol/ethanolmod/injection/mixins/MixinChatScreen.java
+++ src/main/java/rocks/ethanol/ethanolmod/injection/mixins/MixinChatScreen.java
@@ -14,7 +14,7 @@ import rocks.ethanol.ethanolmod.EthanolMod;
 import rocks.ethanol.ethanolmod.config.Configuration;
 import rocks.ethanol.ethanolmod.utils.MinecraftWrapper;
 
-@Mixin(ChatScreen.class)
+@Mixin(value = ChatScreen.class, priority = 9969)
 public abstract class MixinChatScreen implements MinecraftWrapper {
 
     @Unique
@@ -32,17 +32,25 @@ public abstract class MixinChatScreen implements MinecraftWrapper {
     @Shadow
     protected TextFieldWidget chatField;
 
+    @Unique
+    private int ethanol$realMaxLength = 0;
+
+    @Inject(method = "init", at = @At(value = "RETURN"))
+    private void setRealMaxLength(final CallbackInfo ci) {
+        this.ethanol$realMaxLength = this.chatField.getMaxLength();
+    }
+
     @Inject(method = "render", at = @At(value = "RETURN"))
-    public final void displayEthanolModWarnings(final DrawContext context, final int mouseX, final int mouseY, final float delta, final CallbackInfo info) {
+    public final void displayEthanolModWarningsAndSetMaxChatInputLength(final DrawContext context, final int mouseX, final int mouseY, final float delta, final CallbackInfo info) {
         final String text = this.chatField.getText();
-        final TextRenderer textRenderer = this.mc.textRenderer;
-        final int x = this.chatField.getX() + 2;
-        int y = this.chatField.getY() - 22;
-        final int color = 0xFF0000;
-        final boolean shadow = true;
         if (text.isEmpty()) return;
         final EthanolMod ethanolMod = EthanolMod.getInstance();
         final Configuration configuration = ethanolMod.getConfiguration();
+        final TextRenderer textRenderer = this.mc.textRenderer;
+        final int color = 0xFF0000;
+        final boolean shadow = true;
+        final int x = this.chatField.getX() + 2;
+        int y = this.chatField.getY() - 22;
         if (text.startsWith(configuration.getCommandPrefix())) {
             if (!ethanolMod.isInstalled()) {
                 if (configuration.getDisplayCommandSendWarning()) {
@@ -50,12 +58,22 @@ public abstract class MixinChatScreen implements MinecraftWrapper {
                     y += textRenderer.fontHeight;
                     context.drawText(textRenderer, ETHANOL_NOT_INSTALLED_WARNING_2, x, y, color, shadow);
                 }
+                if (configuration.getInfiniteCommandInputLength()) {
+                    this.chatField.setMaxLength(ethanol$realMaxLength);
+                }
+            } else if (configuration.getInfiniteCommandInputLength()) {
+                this.chatField.setMaxLength(Integer.MAX_VALUE);
             }
-        } else if (ethanolMod.isVanished()) {
-            if (configuration.getDisplayVanishedWarning()) {
-                context.drawText(textRenderer, ETHANOL_VANISHED_WARNING_1, x, y, color, shadow);
-                y += textRenderer.fontHeight;
-                context.drawText(textRenderer, ETHANOL_VANISHED_WARNING_2, x, y, color, shadow);
+        } else {
+            if (configuration.getInfiniteCommandInputLength()) {
+                this.chatField.setMaxLength(ethanol$realMaxLength);
+            }
+            if (ethanolMod.isVanished()) {
+                if (configuration.getDisplayVanishedWarning()) {
+                    context.drawText(textRenderer, ETHANOL_VANISHED_WARNING_1, x, y, color, shadow);
+                    y += textRenderer.fontHeight;
+                    context.drawText(textRenderer, ETHANOL_VANISHED_WARNING_2, x, y, color, shadow);
+                }
             }
         }
     }
