532b710eaf613e36de02fe04d6e4d966829f1264
NekosAreKawaii <admin@nekosarekawaii.de>
2024-07-29 03:58:06 +0200

    Moved command handling into mixin

--- src/main/java/rocks/ethanol/ethanolmod/eventhandler/EventInitializer.java
+++ src/main/java/rocks/ethanol/ethanolmod/eventhandler/EventInitializer.java
@@ -1,10 +1,8 @@
 package rocks.ethanol.ethanolmod.eventhandler;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import rocks.ethanol.ethanolmod.eventhandler.impl.AllowChatEventHandler;
 import rocks.ethanol.ethanolmod.eventhandler.impl.DisconnectEventHandler;
 import rocks.ethanol.ethanolmod.eventhandler.impl.HudRenderEventHandler;
 import rocks.ethanol.ethanolmod.eventhandler.impl.JoinEventHandler;
@@ -16,7 +14,6 @@ public class EventInitializer {
         ClientPlayConnectionEvents.DISCONNECT.register(new DisconnectEventHandler());
         ClientPlayConnectionEvents.JOIN.register(new JoinEventHandler());
         HudRenderCallback.EVENT.register(new HudRenderEventHandler());
-        ClientSendMessageEvents.ALLOW_CHAT.register(new AllowChatEventHandler());
         ClientTickEvents.START_WORLD_TICK.register(new WorldTickEventHandler());
     }
 
